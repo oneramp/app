dd975fd7efa4d9926a16f6888c2f397904eefdb7
Jovan Mwesigwa <jovanmwesigwa79@gmail.com>
2025-07-15 15:37:50 +0300

    Refactor ExchangeRateComponent to use utility class for conditional styling and update MainTabsSwitch to reset amount based on selected tab.

--- components/main-tabs-switch.tsx
+++ components/main-tabs-switch.tsx
@@ -4,11 +4,13 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { SwapPanel } from "@/app/components/SwapPanel";
 import { BuyPanel } from "@/app/components/BuyPanel";
 import { useUserSelectionStore } from "@/store/user-selection";
+import { useAmountStore } from "@/store/amount-store";
 
 export function MainTabsSwitch() {
   const { updateSelection } = useUserSelectionStore();
+  const { setAmount } = useAmountStore();
 
-  const washTheseFields = () => {
+  const washTheseFields = (goingToBuy: boolean) => {
     updateSelection({
       country: undefined,
       asset: undefined,
@@ -17,6 +19,10 @@ export function MainTabsSwitch() {
       institution: undefined,
       pastedAddress: undefined,
     });
+
+    if (goingToBuy) {
+      setAmount("1");
+    }
   };
 
   return (
@@ -24,14 +30,14 @@ export function MainTabsSwitch() {
       <TabsList className="grid w-[250px] mx-auto grid-cols-2 p-1 bg-transparent rounded-full mb-2 h-12">
         <TabsTrigger
           value="Swap"
-          onClick={washTheseFields}
+          onClick={() => washTheseFields(false)}
           className="data-[state=active]:bg-neutral-800 text-sm data-[state=active]:text-white data-[state=active]:font-bold text-neutral-400 rounded-full transition-all"
         >
           Swap
         </TabsTrigger>
         <TabsTrigger
           value="Buy"
-          onClick={washTheseFields}
+          onClick={() => washTheseFields(true)}
           className="data-[state=active]:bg-neutral-800 text-sm data-[state=active]:text-white data-[state=active]:font-bold text-neutral-400 rounded-full transition-all"
         >
           Buy
