b87fa87b8a6550a279ef3d3af972fe0ac0023fe8
eliashezron <eliashezron23@gmail.com>
2025-07-21 21:44:47 +0300

    minor updates

--- config/index.tsx
+++ config/index.tsx
@@ -12,8 +12,8 @@ import type { AppKitNetwork } from "@reown/appkit/networks";
 
 // Get projectId from https://cloud.reown.com
 export const projectId =
-  process.env.REOWN_PROJECT_ID || "72d9f10ab6e67c3e8ed81cb9fc7d6c29";
-// process.env.NEXT_PUBLIC_PROJECT_ID || "b56e18d47c72ab683b10814fe9495694"; // this is a public projectId only to use on localhost
+  process.env.REOWN_PROJECT_ID || "2c01570e7f89c66b6977871f86373a28";
+
 
 if (!projectId) {
   throw new Error("Project ID is not defined");
